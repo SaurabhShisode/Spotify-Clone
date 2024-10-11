2948efd0decca47f80a70439caed6d5b0878ee3b
SaurabhShisode <119099005+SaurabhShisode@users.noreply.github.com>
2024-10-11 22:38:41 +0530

    initialized js

---
+++ script.js
@@ -0,0 +1,9 @@
+
+async function main(){
+    let a  = await fetch("http://127.0.0.1:3000/songs/")
+    let response = await a.text();
+    console.log(response)
+    let div = document.createElement("div")
+    
+
+}
