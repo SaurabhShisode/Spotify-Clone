bf6f26d642fc238f3851f2b56e6c58de98a1e1f3
SaurabhShisode <119099005+SaurabhShisode@users.noreply.github.com>
2024-10-16 21:20:26 +0530

    created song list and playbar and atached them with event listeners

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log('Lets write JavaScript');
+let currentSong = new Audio();
 async function getSongs() {
     // Fetch from the "songs" subfolder
     let a = await fetch("http://127.0.0.1:5500/Spotify-Clone/songs/")
@@ -17,16 +18,58 @@ async function getSongs() {
     return songs
 }
 
-async function main(){
+const playMusic = (track) => {
+    currentSong.src = "songs/" + track
+    currentSong.play()
+    play.src = "img/pause.svg"
+    document.querySelector(".songinfo").innerHTML = decodeURI(track)
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
+}
+
+async function main() {
+
+
+    //get the list of all the songs
     let songs = await getSongs()
     console.log(songs)
 
-    // Play the first song if available
-    var audio = new Audio(songs[0]);
-    audio.play();
+    //show all the songs in the playlist
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
+    for (const song of songs) {
+        let songName = song.split("/").pop(); // Extract the song name without the "songs/" prefix
+        songUL.innerHTML = songUL.innerHTML + `<li> <img class="invert" src="img/music.svg" alt="">
+                            <div class="info">
+                                <div>${decodeURIComponent(songName)} </div>
+                                <div>SmoKey</div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>
+                                <img class="invert" src="img/play.svg" alt="">
+                            </div></li>`;
 
-    audio.addEventListener("loadeddata", () =>{
-        console.log(audio.duration, audio.currentSrc, audio.currentTime)
-    });
+    }
+    // Attach an event listener to each song
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element => {
+            playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
+
+        })
+    })
+
+    //Attach an event listener to play, next and previous
+    play.addEventListener("click", () => {
+        if (currentSong.paused) {
+            currentSong.play()
+            play.src = "img/pause.svg"
+        }
+        else {
+            currentSong.pause()
+            play.src = "img/play.svg"
+        }
+    })
+    return songs
+
 }
+
+
 main()
